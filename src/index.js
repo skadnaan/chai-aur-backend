51c31f85439e9fb4af11c0f1ac957d370ca3980b
skadnaan <skadnaanahmed34@gmail.com>
2025-09-10 19:11:07 +0530

    connect to mogo atlas

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+/*  FIRST APPROACH TO WRITE AND CONNECT TO DB : 
+
+const app = express();
+
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR" , error);
+            throw error
+            
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.log("ERROR", error);
+        throw error
+        
+    }
+})() */
