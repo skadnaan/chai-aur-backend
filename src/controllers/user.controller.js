b19416c24c7de429f95d626601b3753772278420
skadnaan <skadnaanahmed34@gmail.com>
2025-09-22 15:51:43 +0530

    refactor: update response format in getCurrentUser controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -245,7 +245,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "Current user fetched successfully");
+    .json(new apiResponse(200, req.user, "Current user fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
